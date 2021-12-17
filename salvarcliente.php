43b4159616a19599f3e9d8062c02545d807a0fbe
julliaax <93981803+julliaax@users.noreply.github.com>
2021-12-17 16:22:00 -0300

    Add files via upload

---
+++ salvarcliente.php
@@ -0,0 +1,39 @@
+<?php
+
+
+$nomeCliente = $_GET["nome"];
+$cpfCliente = $_GET["cpf"];
+$foneCliente = $_GET["fone"];
+$emailCliente = $_GET["email"];
+
+
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Salvar Cliente</title>
+</head>
+<body>
+    <table border="1">
+        <tr>
+            <td>Nome Completo</td>
+            <td><?php echo $nomeCliente; ?></td>
+
+            <tr>
+            <td>CPF</td>
+            <td><?php echo $cpfCliente; ?></td>
+
+            <tr>
+            <td>Telefone</td>
+            <td><?php echo $foneCliente; ?></td>
+
+            <tr>
+            <td>email</td>
+            <td><?php echo $emailCliente; ?></td>
+        </tr>
+    </table>
+</body>
+</html>
